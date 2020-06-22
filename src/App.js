c4aa9aa4d09c07119c13aaae05480aeb85614cc5
Ravi Jagani <jaganiravi96@gmail.com>
2020-06-22 20:03:56 +0530

    javascript changes test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello React
-      </header>
+    <div class="App">
+      <h1>  Hello React  </h1>    
     </div>
   );
 }
