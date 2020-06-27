b8170d4b718ede3248b0248579256bfbcce0491b
Ravi Jagani <jaganiravi96@gmail.com>
2020-06-27 23:37:25 +0530

    Creating the project file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-//import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div class="App">
-      <h1>  Hello React  </h1>    
-    </div>
-  );
+//here we are creating react component so it will be created by class
+//here will give the same name of class as we have file name 
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p> My name is Ravi, I am software engineer</p>
+                <p> I am looking for the meaningful project</p>
+            </div>
+        )
+    }
 }
 
 export default App;
