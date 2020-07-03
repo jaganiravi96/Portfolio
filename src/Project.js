75d4780dc98c909e8cef4d881758248f7abdad93
Ravi Jagani <jaganiravi96@gmail.com>
2020-07-03 08:52:14 +0530

    Adding the project.js component

---
+++ src/Project.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react.js';
+import PROJECTS from './data/project.js';
+
+class Project extends Components{
+
+    render(){
+        return(
+            <div>
+                <h2>Highlighted Project</h2>
+                <div>
+                    <div>{}</div>
+                </div>
+            </div>
+        )
+    }
+
+}
